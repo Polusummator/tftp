25864d1487991602782046b2050492d5aa2ddb99
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2025-07-08 17:25:45 +0300

    RQ packets pack/unpack

--- packet.go
+++ packet.go
@@ -1,5 +1,10 @@
 package tftp
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 const (
 	opRRQ   = uint16(1)
 	opWRQ   = uint16(2)
@@ -20,24 +25,52 @@ RRQ/WRQ packet
 type packetRRQ []byte
 type packetWRQ []byte
 
-func packRQ(packet []byte, opcode uint16, filename string, mode string) int {}
+func packRQ(opcode uint16, filename string, mode string) []byte {
+	packet := make([]byte, 4+len(filename)+len(mode))
+	binary.BigEndian.PutUint16(packet[0:], opcode)
+	copy(packet[2:], filename)
+	copy(packet[3+len(filename):], mode)
+	return packet
+}
 
-func unpackRQ(packet []byte) (opcode uint16, filename string, mode string, err error) {}
+func unpackRQ(packet []byte) (filename string, mode string, err error) {
+	opcode := binary.BigEndian.Uint16(packet[0:])
+	if opcode < 1 || opcode > 5 {
+		return "", "", fmt.Errorf("invalid opcode: %d", opcode)
+	}
+	filenameEndPos := 2
+	for filenameEndPos < len(packet) && packet[filenameEndPos] != 0 {
+		filenameEndPos++
+	}
+	if filenameEndPos == len(packet) {
+		return "", "", fmt.Errorf("invalid RQ filename format")
+	}
+	filename = string(packet[2:filenameEndPos])
+	modeEndPos := 3 + len(filename)
+	for modeEndPos < len(packet) && packet[modeEndPos] != 0 {
+		modeEndPos++
+	}
+	if modeEndPos != len(packet)-1 {
+		return "", "", fmt.Errorf("invalid RQ mode format")
+	}
+	mode = string(packet[(3 + len(filename)):modeEndPos])
+	return filename, mode, nil
+}
 
-func packRRQ(filename string, mode string) int {
-
+func packRRQ(filename string, mode string) packetRRQ {
+	return packRQ(opRRQ, filename, mode)
 }
 
 func unpackRRQ(packet packetRRQ) (filename string, mode string, err error) {
-
+	return unpackRQ(packet)
 }
 
-func packWRQ(packet []byte, filename string, mode string) int {
-
+func packWRQ(packet []byte, filename string, mode string) packetWRQ {
+	return packRQ(opWRQ, filename, mode)
 }
 
 func unpackWRQ(packet packetWRQ) (filename string, mode string, err error) {
-
+	return unpackRQ(packet)
 }
 
 /*
